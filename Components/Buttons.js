97fcb9ab1535de3ebddb8b4670c3982de65acc13
jamalamar <jamal_ab22@hotmail.com>
2019-09-05 13:51:02 -0500

    created components folder and added a buttons component

---
+++ Components/Buttons.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { Button, StyleSheet, View } from 'react-native';
+
+export default class ButtonBasics extends Component {
+  _onPressButton() {
+    alert('You tapped the button!')
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="Press Me"
+          />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="Press Me"
+            color="#841584"
+          />
+        </View>
+        <View style={styles.alternativeLayoutButtonContainer}>
+          <Button
+            onPress={this._onPressButton}
+            title="This looks great!"
+          />
+          <Button
+            onPress={this._onPressButton}
+            title="OK!"
+            color="#841584"
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+   justifyContent: 'center',
+  },
+  buttonContainer: {
+    margin: 1
+  },
+  alternativeLayoutButtonContainer: {
+    margin: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
+});
