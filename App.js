165fa9c615e332e6f15fae562475fef347738435
jamalamar <jamal_ab22@hotmail.com>
2019-09-05 13:51:34 -0500

    components folder and buttons component

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Buttons from './Components/Buttons.js'
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world!</Text>
+
+      <Text style={styles.title}>Hello World!</Text>
+      <Buttons/>
+    
     </View>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -15,5 +23,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    color: '#000',
+    fontSize: '50px',
+    flexUp: 30,
   },
 });
